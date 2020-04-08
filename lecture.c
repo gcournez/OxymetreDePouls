b14d642f21f9d8cca7e1699453642b947873ac03
EtienneDouillard <edouillard@outlook.com>
2020-04-08 20:18:50 +0200

    Lecture fonctionne manque quelques modifications
    Signed-off-by: EtienneDouillard <edouillard@outlook.com>

--- lecture.c
+++ lecture.c
@@ -7,48 +7,57 @@ absorp lecture(FILE* file_pf,  int* file_state){// poiteur à mon fichier et poi
     absorp myAbsorp={0};
     char actualCaractere = 0;
     int i =0; //indice
-    float tramDataStr[21]; //Tableau pour stocker chaque caractère de la tram, taille d'une tram = 21 caratèrere
+    char tramDataStr[21]; //Tableau pour stocker chaque caractère de la tram, taille d'une tram = 21 caratèrere
 
 
-    if(file_pf == NULL) { //vérification du le fichier est bien lu
+    if(file_pf == NULL) { //vérification que le fichier est bien lu
         *file_state = EOF; // fin de fichier
         printf("Pas de fichier en entrée de lecture");//Affiche une erreur de lecture de fichier
     }else{
         while (*file_state != EOF){ // boucle pour traverser tout le fichier.
             while (i<21){ //boucle pour obetenir les 21 caractères de la tram
                 actualCaractere = (char)fgetc(file_pf); //lecture caractère par caractère
-                printf("%c",actualCaractere); //test print values
+                //printf("%c",actualCaractere); //test print values
 
-                tramDataStr[i]= atof(actualCaractere);//remplir la tram dans le tableau
+                tramDataStr[i]= (actualCaractere);//remplir la tram dans le tableau
 
-                printf("%f",tramDataStr[i]);
 
-                if (strcmp("\n", &actualCaractere) == 0){ //si on arrive à la fin de la ligne
-                    *file_state+=21; //nombre
-                    i=21;
-                }else if (actualCaractere == EOF){ //arrivée au dernie caractère
+
+                if (strcmp("\r", &actualCaractere) == 0) { //si on arrive à la fin de la ligne
+                    //actualCaractere = (char)fgetc(file_pf); //passage à la ligne suivante
+                    *file_state += 21; // le nombre de trame en faisant file_state%21
+                    i = 21;
+
+                }else if(actualCaractere == EOF){ //arrivée au dernie caractère
                     *file_state = EOF; //fin de fichier
-
                 }
-
-                i+=1;
+                i+=1;//incrémentation
             }
+            i=0;//Revenir au début du tableau
 
-            i=0;
 
-            myAbsorp.acr =tramDataStr[0] *1000 + tramDataStr[1] * 100 + tramDataStr[2] * 10 + tramDataStr[3];
-            myAbsorp.dcr =tramDataStr[0] *1000 + tramDataStr[1] * 100 + tramDataStr[2] * 10 + tramDataStr[3];
-            myAbsorp.acir = tramDataStr[0] *1000 + tramDataStr[1] * 100 + tramDataStr[2] * 10 + tramDataStr[3];
-            myAbsorp.dcir = tramDataStr[0] *1000 + tramDataStr[1] * 100 + tramDataStr[2] * 10 + tramDataStr[3];
+            /*          Exemple format de du tabelau tramDatastr
+             *     ____________________________________________________________________________________
+             *    [     ACr      | , |       DCr     | , |      ACir     | , |   DCir       | LF | RC ]
+             *   [2 | 0 | 8 | 5 | , | 2 | 0 | 3 | 0 | , | 2 | 0 | 2 | 7 | , | 2 | 0 | 3 | 0 |   |    ] = tramDataStr
+             *  [32| 30| 38| 35| 2C| 32| 30| 33| 30| 2C| 32| 30| 33| 37| 2C| 32| 30| 33| 30|   |    ]
+             *
+            */
+            //-48 pour passer de ASCII à un nombre entier.
+            myAbsorp.acr = (float)((tramDataStr[0]-48)*1000 + (tramDataStr[1]-48)*100 + (tramDataStr[2]-48) * 10 + (tramDataStr[3]-48));
+            myAbsorp.dcr = (float)((tramDataStr[5]-48)*1000 + (tramDataStr[6]-48)*100 + tramDataStr[7] * 10 + tramDataStr[8]-48);
+            myAbsorp.acir =(float) ((tramDataStr[10]-48) *1000 + (tramDataStr[11]-48) * 100 + (tramDataStr[12]-48) * 10 +( tramDataStr[13]-48));
+            myAbsorp.dcir =(float)(( tramDataStr[15] -48)*1000 + (tramDataStr[16]-48) * 100 + (tramDataStr[17]-48) * 10 +( tramDataStr[18]-48));
 
             myAbsorp.acir -=2048 ; //Centrer acir en 0 car avant elles ocillaient entre 0 et 4096
             myAbsorp.acr -= 2048 ;//centrer acr en 0
+
+            printf("ACr = %f, DCr = %f, ACir = %f, DCir= %f \n",myAbsorp.acr, myAbsorp.dcr, myAbsorp.acir, myAbsorp.dcir);
 
 
         }
     finFichier(file_pf);
     }
-
 
 
 	return myAbsorp; // return EOF flag
